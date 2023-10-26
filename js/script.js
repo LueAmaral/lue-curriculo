2e103ebe12749bc6f5275a13563ca2c02e5fb36a
Lue <145283442+LueAmaral@users.noreply.github.com>
2023-10-26 17:20:06 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const $html = document.querySelector("html")
+const $toggletheme = document.getElementById("#toggletheme")
+const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+toggletheme.onclick = function(){
+    $html.classList.toggle("dark-mode")
+    if($html.classList.contains("dark-mode")){
+        toggletheme.src = "../img/light_mode.svg"
+    }else{
+        toggletheme.src = "../img/dark_mode.svg"
+    }
+}
+
+if( prefersColorScheme.matches ) {
+    $html.classList.toggle("dark-mode")
+    if($html.classList.contains("dark-mode")){
+        toggletheme.src = "../img/light_mode.svg"
+    }else{
+        toggletheme.src = "../img/dark_mode.svg"
+    }
+}
