306fb62e5ea7a50a32d36f70b0673717ecde77ee
Lue <145283442+LueAmaral@users.noreply.github.com>
2023-10-26 17:22:30 -0300

    Correção do src

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 toggletheme.onclick = function(){
     $html.classList.toggle("dark-mode")
     if($html.classList.contains("dark-mode")){
-        toggletheme.src = "../img/light_mode.svg"
+        toggletheme.src = "img/light_mode.svg"
     }else{
-        toggletheme.src = "../img/dark_mode.svg"
+        toggletheme.src = "'img/dark_mode.svg"
     }
 }
 
 if( prefersColorScheme.matches ) {
     $html.classList.toggle("dark-mode")
     if($html.classList.contains("dark-mode")){
-        toggletheme.src = "../img/light_mode.svg"
+        toggletheme.src = "img/light_mode.svg"
     }else{
-        toggletheme.src = "../img/dark_mode.svg"
+        toggletheme.src = "img/dark_mode.svg"
     }
 }
